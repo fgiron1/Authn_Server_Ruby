7da7d212a4137836ca86c07d3571d743467cedc9
Fernando <fernandogirondominguez@hotmail.com>
2021-08-12 20:22:10 +0200

    Installed SQL Server adapter

---
+++ app/models/candidato_model.rb
@@ -0,0 +1,2 @@
+class CandidatoModel < ApplicationRecord
+end
---
+++ db/migrate/20210812174905_create_candidato_models.rb
@@ -0,0 +1,8 @@
+class CreateCandidatoModels < ActiveRecord::Migration[6.1]
+  def change
+    create_table :candidato_models do |t|
+
+      t.timestamps
+    end
+  end
+end
